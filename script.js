3a9303a64a91fa63359027839408feffa23fa92a
joy012 <67516270+joy012@users.noreply.github.com>
2020-07-25 18:01:00 +0600

    duplicate code fixed

--- script.js
+++ script.js
@@ -1,104 +1,85 @@
-
-// deducting and update item number and current item total price
-function deduct(itemName,itemCount_id,itemPrice_id){
-    let unitPrice = 0;
-    const itemTotalPrice_id = itemPrice_id;
-    if(itemName === 'phn'){
-        unitPrice = 1219;
-    }
-    else{
-        unitPrice = 59;
-    }
-    let currentCount = parseInt(document.getElementById(itemCount_id).value);
-    if(currentCount > 0 ){
-        document.getElementById(itemCount_id).value = --currentCount;
-
-        updateCurrentItemPrice(itemTotalPrice_id,unitPrice,currentCount);
-        updateCostSection(-1 * unitPrice);
-    }
+// get current product count
+function getProductCount(product){
+    return parseInt(document.getElementById(product + '-count').value);
 }
 
-// adding and updating item number and current price
-function add(itemName,itemCount_id,itemPrice_id){
-    let unitPrice = 0;
-    const itemTotalPrice_id = itemPrice_id;
-    if(itemName === 'phn'){
-        unitPrice = 1219;
+// hand increase and decrease item value and cost
+function handleProductChange(product,isIncrease){
+    const unitPrice = (product === 'phone')? 1219 : 59 ;
+    let currentCount = getProductCount(product);
+    if(isIncrease == false && currentCount > 0 ){
+        currentCount--;
     }
-    else{
-        unitPrice = 59;
+    else if(isIncrease == true){
+        currentCount++;
     }
-    let currentCount = parseInt(document.getElementById(itemCount_id).value);
-    document.getElementById(itemCount_id).value = ++currentCount;
-
-    const itemUpdatedPrice = updateCurrentItemPrice(itemTotalPrice_id,unitPrice,currentCount);
-    updateCostSection(unitPrice);
-}
-
-// update item current price
-function updateCurrentItemPrice(itemPrice_id,unitPrice,currentCount){
+    document.getElementById(product + '-count').value = currentCount;
     const updatedPrice = unitPrice * currentCount;
-    document.getElementById(itemPrice_id).innerText = updatedPrice.toLocaleString();
-}
-// update costs section
-function updateCostSection(unitPrice){
-    const subTotal = parseFloat((document.getElementById('subtotal').innerText).split(",").join(""));
-    const updatedSubtotal = subTotal + unitPrice;
-    document.getElementById('subtotal').innerText = updatedSubtotal.toLocaleString();
+    document.getElementById(product + '-total').innerText = updatedPrice.toLocaleString();
+    updateCostSection();
 
-    const updatedTax = parseFloat(((updatedSubtotal * 0.08).toFixed(2)).split(",").join(""));
+}
+
+// update costs section
+function updateCostSection(){
+    const phoneCount = getProductCount('phone');
+    const caseCount = getProductCount('case');
+
+    const updatedSubtotal =(phoneCount * 1219) + (caseCount * 59);
+    document.getElementById('subtotal').innerText = updatedSubtotal.toLocaleString();
+    
+    const updatedTax = Math.round(updatedSubtotal * 0.08);
     document.getElementById('tax').innerText = updatedTax.toLocaleString();
 
-    const updatedTotal = parseFloat(((updatedSubtotal + updatedTax).toFixed(2)).split(",").join(""));
+    const updatedTotal = updatedSubtotal + updatedTax;
     document.getElementById('total').innerText = updatedTotal.toLocaleString();
 }
 
 // costs after remove one item from cart
-function afterRemove(unitPrice,id){
-    const itemNum = parseInt(document.getElementById(id).value);
+function afterRemove(product){
+    document.getElementById('cart-' + product).style.display = 'none';
+    const itemNum = getProductCount(product);
+    const unitPrice = (product === 'phone')? 1219 : 59 ;
 
     const subTotal = parseFloat((document.getElementById('subtotal').innerText).split(",").join(""));
     const updatedSubtotal = subTotal - (itemNum*unitPrice);
     document.getElementById('subtotal').innerText = updatedSubtotal.toLocaleString();
 
-    const updatedTax = parseFloat(((updatedSubtotal * 0.08).toFixed(2)).split(",").join(""));
+    const updatedTax = Math.round(updatedSubtotal * 0.08);
     document.getElementById('tax').innerText = updatedTax.toLocaleString();
 
-    const updatedTotal = parseFloat(((updatedSubtotal + updatedTax).toFixed(2)).split(",").join(""));
+    const updatedTotal = updatedSubtotal + updatedTax;
     document.getElementById('total').innerText = updatedTotal.toLocaleString();
 }
 
-
 // phone minus button event handler
 document.getElementById('decreasePhone').addEventListener('click', function(){
-    deduct('phn','currentPhoneCount','phoneTotalPrice');
+    handleProductChange('phone',false);
 })
 
 // phone plus button event handler
 document.getElementById('increasePhone').addEventListener('click', function(){
-    add('phn','currentPhoneCount','phoneTotalPrice');
+    handleProductChange('phone',true);
 })
 
 // case minus button event handler
 document.getElementById('decreaseCase').addEventListener('click', function(){
-    deduct('case','currentCaseCount','caseTotalPrice');
+    handleProductChange('case', false);
 })
 
 // case plus button event handler
 document.getElementById('increaseCase').addEventListener('click', function(){
-    add('case','currentCaseCount','caseTotalPrice');
+    handleProductChange('case',true);
 })
 
 // phone item remove button click handler
 document.querySelectorAll('img.remove-item')[0].addEventListener('click', function(){
-    document.getElementById('cart-phone').style.display = 'none';
-    afterRemove(1219,'currentPhoneCount');
+    afterRemove('phone');
 })
 
 // Case item remove button click handler
 document.querySelectorAll('img.remove-item')[1].addEventListener('click', function(){
-    document.getElementById('cart-case').style.display = 'none';
-    afterRemove(59,'currentCaseCount');
+    afterRemove('case');
 })
 
 // checkout button click handler
@@ -113,7 +94,6 @@ document.getElementById('placeOrder').addEventListener('click', function(){
     document.getElementById('form').style.display = 'none';
     document.getElementById('thankYou').style.display = 'block';
     document.body.style.backgroundColor = '#FCF6F5FF';
-    const customer = document.getElementById('inputName').value;
-    const upCaseName = customer.toUpperCase(); 
-    document.querySelector('#thankYou h1').innerText = 'Thank you ' + upCaseName + ' for shopping with Panda Commerce!';
+    const customer = (document.getElementById('inputName').value).toUpperCase(); 
+    document.querySelector('#thankYou h1').innerText = `Thank you ${customer} for shopping with Panda Commerce!`;
 })
